c15d6fff53949f0980515a2ace577264ca6501b0
miv <mirafamare4@gmail.com>
2024-05-29 18:11:03 -0700

    adding api connnections

---
+++ lib/Api_Connections/Api_Connections.dart
@@ -0,0 +1,8 @@
+class API {
+  static const validateEmail = "http://10.240.68.103/Delivary_app_project/user/validateEmail.php";
+  // static const hostConnectUser = "$hostConnect/user"
+   static const signUp = "http://10.240.68.103//Delivary_app_project/user/signUp.php";
+  // static const validateEmail = "$hostConnectUser/validateEmail.php";
+  static const ProductApi = 'http://10.240.68.103/delivery-web/api/data_serve.php';
+
+}
